bb06cc01054aa2813a3b5602183c8a5017c721d2
rowlandmayowa527-ai <rowlandmayowa527@gmail.com>
2025-10-15 20:28:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ export const GITHUB_USERNAME = "rowlandmayowa527-ai"; // your GitHub username
 export const REPO_NAME = "pfl-livescore-center";     // your repo
 export const FILE_PATH = "data.json";               // file you want to update
 export const BRANCH = "main";                       // branch name
-export const TOKEN = "YOUR_PERSONAL_ACCESS_TOKEN";  // paste your PAT here
+export const TOKEN = "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";  // paste your PAT here
