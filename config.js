4011f86784d8174750cc85545a694fe7ae217373
rowlandmayowa527-ai <rowlandmayowa527@gmail.com>
2025-10-15 22:04:00 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-export const GITHUB_USERNAME = "rowlandmayowa527-ai"; // your GitHub username
-export const REPO_NAME = "pfl-livescore-center";     // your repo
-export const FILE_PATH = "data.json";               // file you want to update
-export const BRANCH = "main";                       // branch name
-export const TOKEN = "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";  // paste your PAT here
